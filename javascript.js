2f51b0dc13875deb937cc500d1c5de6580746645
Positiviste <106838114+Positiviste@users.noreply.github.com>
2023-01-19 10:33:38 +0100

    When mouse enter a block, paint it in black

--- javascript.js
+++ javascript.js
@@ -1,27 +1,36 @@
 const grid = document.querySelector('#grid');
 
-function createBox () {
+function createBox() {
     const box = document.createElement('span');
     box.className = 'box';
     grid.lastChild.appendChild(box);
 }
 
-function createRow () {
+function createRow() {
     const row = document.createElement('span');
     row.className = 'row';
     grid.appendChild(row);
 }
 
 
-function createGrid (row, column) {
-    for (let x = 1 ; x <= row ; x++) {
+function createGrid(row, column) {
+    for (let x = 1; x <= row; x++) {
         createRow();
-        for (let y = 1 ; y <= column ; y++){
+        for (let y = 1; y <= column; y++) {
             createBox();
         }
     }
 }
 
 // rangée - colonne
-createGrid(5,10);
+createGrid(5, 10);
 
+// Paint in black on mouse enter
+const listBox = document.querySelectorAll('.box');
+console.log(listBox);
+
+Array.from(listBox).forEach(box => {
+    box.addEventListener('mouseenter', function(box) {
+        this.style.backgroundColor = '#000';
+    });
+});
